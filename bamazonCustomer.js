e3c099888fd5d92ec01150beaf6927a722f8d086
SheetalSrikumar <bs.sheetal@gmail.com>
2018-12-12 19:09:27 -0800

    able to retreive table data

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,40 @@
-var sql=require("mysql");
-var inquirer=require("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var cTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
+
+});
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, response) {
+        if (err) {
+            throw err
+        };
+        console.table(response);
+    });
+}
+
+inquirer.prompt([
+    {
+        type:"input",
+        message:"",
+    },
+])
