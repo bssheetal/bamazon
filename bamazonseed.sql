f9ef12581a494105edb97813001d8b7bca0f1125
SheetalSrikumar <bs.sheetal@gmail.com>
2018-12-13 20:56:02 -0800

    updated the sql seed file eith 10rows

--- bamazonseed.sql
+++ bamazonseed.sql
@@ -12,8 +12,18 @@ stock_quantity INTEGER(10),
 PRIMARY KEY(item_id)
 );
 
-select * from products;
-
 INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Finish dishwasher Gelpacks","Health & Household",10.95,10);
 INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Philips Soft White Bulb","Health & Household",11.46,50);
-INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Tea Tree Oil FotSoak","Health & Nutrition",12.95,100);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Tea Tree Oil FootSoak","Health & Nutrition",12.95,100);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Blink Security Camera","Electronics",90,100);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("HeadFirst Javascript","Books",52,10);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("HeadFirst Java","Books",52,10);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Logitech Mouse","Electronics",10,10);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Logitech Keyboard","Electronics",10,10);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Upper Bounce Trampolines","Sports & Fitness",69.74,10);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Puma Golf Shoes","Sports & Fitness",86,10);
+
+
+
+select * from products;
+
